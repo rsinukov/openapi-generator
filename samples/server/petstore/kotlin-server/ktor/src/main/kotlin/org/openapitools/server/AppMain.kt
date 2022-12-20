5c39bab6ed4f9cc16f28ff77b2683b13dcf03397
rsinukov <rxsinukov@gmail.com>
2022-12-20 13:10:58 +0100

    [Ktor] Update generator to latest Ktor version #14061

--- samples/server/petstore/kotlin-server/ktor/src/main/kotlin/org/openapitools/server/AppMain.kt
+++ samples/server/petstore/kotlin-server/ktor/src/main/kotlin/org/openapitools/server/AppMain.kt
@@ -1,20 +1,25 @@
 package org.openapitools.server
 
-import com.codahale.metrics.Slf4jReporter
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.gson.*
+import io.ktor.server.application.*
+import io.ktor.serialization.gson.*
 import io.ktor.http.*
-import io.ktor.locations.*
-import io.ktor.metrics.dropwizard.*
+import io.ktor.server.resources.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.autohead.*
+import io.ktor.server.plugins.conditionalheaders.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.hsts.*
+import com.codahale.metrics.Slf4jReporter
+import io.ktor.server.metrics.dropwizard.*
 import java.util.concurrent.TimeUnit
-import io.ktor.routing.*
-import io.ktor.util.*
+import io.ktor.server.routing.*
 import com.typesafe.config.ConfigFactory
-import io.ktor.auth.*
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
-import io.ktor.config.HoconApplicationConfig
+import io.ktor.server.config.HoconApplicationConfig
+import io.ktor.server.auth.*
 import org.openapitools.server.infrastructure.*
 import org.openapitools.server.apis.PetApi
 import org.openapitools.server.apis.StoreApi
@@ -27,13 +32,11 @@ object HTTP {
     val client = HttpClient(Apache)
 }
 
-@KtorExperimentalAPI
-@KtorExperimentalLocationsAPI
 fun Application.main() {
     install(DefaultHeaders)
     install(DropwizardMetrics) {
         val reporter = Slf4jReporter.forRegistry(registry)
-            .outputTo(log)
+            .outputTo(this@main.log)
             .convertRatesTo(TimeUnit.SECONDS)
             .convertDurationsTo(TimeUnit.MILLISECONDS)
             .build()
@@ -43,9 +46,11 @@ fun Application.main() {
         register(ContentType.Application.Json, GsonConverter())
     }
     install(AutoHeadResponse) // see https://ktor.io/docs/autoheadresponse.html
+    install(ConditionalHeaders) // see https://ktor.io/docs/conditional-headers.html
     install(Compression, ApplicationCompressionConfiguration()) // see https://ktor.io/docs/compression.html
+    install(CORS, ApplicationCORSConfiguration()) // see https://ktor.io/docs/cors.html
     install(HSTS, ApplicationHstsConfiguration()) // see https://ktor.io/docs/hsts.html
-    install(Locations) // see https://ktor.io/docs/features-locations.html
+    install(Resources)
     install(Authentication) {
         // "Implement API key auth (api_key) for parameter name 'api_key'."
         apiKeyAuth("api_key") {
@@ -58,7 +63,7 @@ fun Application.main() {
         }
         oauth("petstore_auth") {
             client = HttpClient(Apache)
-            providerLookup = { ApplicationAuthProviders["petstore_auth"] }
+            providerLookup = { applicationAuthProvider(this@main.environment.config) }
             urlProvider = { _ ->
                 // TODO: define a callback url here.
                 "/"
